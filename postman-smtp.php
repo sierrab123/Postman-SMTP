d14e234cfba9ae3ee08a0367ac428880c41bfd98
Yehuda Hassine <yehuda@myinbox.in>
2017-10-12 23:23:42 +0300

    Change plugin headers
    Jason is not answering :-(

--- postman-smtp.php
+++ postman-smtp.php
@@ -4,10 +4,10 @@
  * Plugin Name: Postman SMTP
  * Plugin URI: https://wordpress.org/plugins/postman-smtp/
  * Description: Email not reliable? Postman is the first and only WordPress SMTP plugin to implement OAuth 2.0 for Gmail, Hotmail and Yahoo Mail. Setup is a breeze with the Configuration Wizard and integrated Port Tester. Enjoy worry-free delivery even if your password changes!
- * Version: 1.7.2
- * Author: Jason Hendriks
+ * Version: 1.7.3
+ * Author: Jason Hendriks, Yehuda Hassine
  * Text Domain: postman-smtp
- * Author URI: http://www.codingmonkey.ca
+ * Author URI: https://github.com/yehudah/Postman-SMTP
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
@@ -21,7 +21,6 @@
 // filter print_postman_status: apply this filter to print the human-readable plugin state
 // filter postman_module: implement this filter and return the instance of the module
 // filter postman_register_modules: apply this filter to register the module
-
 // TODO v1.7
 // -- Postmark API http://plugins.svn.wordpress.org/postmark-approved-wordpress-plugin/trunk/postmark.php
 // -- Amazon SES API http://docs.aws.amazon.com/ses/latest/DeveloperGuide/send-email-api.html
@@ -32,15 +31,14 @@
 // -- customize sent-mail icon WordPress dashboard
 // -- multisite support for site-wide email configuration. allow network admin to choose whether subdomains may override with their own settings. subdomains may override with their own settings.
 // -- multiple mailbox support
-
 /**
  * Create the main Postman class to start Postman
  *
- * @param unknown $startingMemory        	
+ * @param unknown $startingMemory
  */
-function postman_start($startingMemory) {
-	postman_setupPostman ();
-	PostmanUtils::logMemoryUse ( $startingMemory, 'Postman' );
+function postman_start( $startingMemory ) {
+	postman_setupPostman();
+	PostmanUtils::logMemoryUse( $startingMemory, 'Postman' );
 }
 
 /**
@@ -48,11 +46,11 @@ function postman_start($startingMemory) {
  */
 function postman_setupPostman() {
 	require_once 'Postman/Postman.php';
-	$kevinCostner = new Postman ( __FILE__, '1.7.2' );
+	$kevinCostner = new Postman( __FILE__, '1.7.2' );
 }
 
 /**
  * Start Postman
  */
-postman_start ( memory_get_usage () );
+postman_start( memory_get_usage() );
 
